bea8b51e14c2298cf46bf731a635374f7f2c6f56
bernardyip <bernard_yip40@hotmail.com>
2017-11-08 16:22:13 +0800

    update functionchange browser plugin function

--- src/browser/VulnerablePluginSrc.js
+++ src/browser/VulnerablePluginSrc.js
@@ -1,5 +1,5 @@
-function runplugin(name, success, error) {
-    success(name);
+function runplugin(success, error, name, name2, name3) {
+    success(name + " : " + name2 + " : " + name3);
 }
 
 module.exports = {
